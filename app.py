51fe6db7f324ddaae4882aafe14921262cebfacd
pranay1859 <53484511+pranay1859@users.noreply.github.com>
2022-06-29 00:51:18 +0700

    add feature and email automation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 from flask import Flask, redirect, render_template, request
+import smtplib
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,14 +19,22 @@ def write_to_csv(data):
         message = data['message']
         file = csv.writer(target, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         file.writerow([name, email, subject, message])
+        return name, email, subject, message
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     try:
         if request.method == 'POST':
             data = request.form.to_dict()
-            write_to_csv(data)
+            name, email, subject, message = write_to_csv(data)
+            bot_message = "\n\n name:{}\n e-mail:{}\n subject {}\n message:{}".format(name, email, subject, message)
+            new_message = 'Subject: {}\n\n{}\n\n this is bot generate {}'.format(subject, message, bot_message)
+            server = smtplib.SMTP("smtp.gmail.com", 587)
+            server.starttls()
+            server.login('pranay1859@gmail.com', 'czyrpjjlfnsizkvp')
+            server.sendmail(email, 'pranay1859@gmail.com', new_message)
             return redirect('/thankyou.html')
-    except:
+    except Exception as e:
+        print(e)
         return 'Please try again, something went wrong'
 
